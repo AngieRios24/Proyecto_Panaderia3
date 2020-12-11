2b76b4ac6540a67cdbbd2d4fb4bf6adc51c44cbb
AngieRios24 <angiekatherine@live.com>
2020-12-11 16:34:00 -0500

    se aceptan

--- panaderia/vendor/symfony/string/Resources/functions.php
+++ panaderia/vendor/symfony/string/Resources/functions.php
@@ -11,7 +11,6 @@
 
 namespace Symfony\Component\String;
 
-<<<<<<< HEAD
 function u(?string $string = ''): UnicodeString
 {
     return new UnicodeString($string ?? '');
@@ -20,29 +19,14 @@ function u(?string $string = ''): UnicodeString
 function b(?string $string = ''): ByteString
 {
     return new ByteString($string ?? '');
-=======
-function u(string $string = ''): UnicodeString
-{
-    return new UnicodeString($string);
-}
-
-function b(string $string = ''): ByteString
-{
-    return new ByteString($string);
->>>>>>> be94746b1f59100ae2b323d591c9213416c268d3
 }
 
 /**
  * @return UnicodeString|ByteString
  */
-<<<<<<< HEAD
 function s(?string $string = ''): AbstractString
 {
     $string = $string ?? '';
 
-=======
-function s(string $string): AbstractString
-{
->>>>>>> be94746b1f59100ae2b323d591c9213416c268d3
     return preg_match('//u', $string) ? new UnicodeString($string) : new ByteString($string);
 }
